8d2d82fd028160b33ca322b693c2507ce98a7e06
Rompet <r.petrov@smartdata.dev>
2025-12-12 22:44:26 +0800

    init backend

---
+++ backend/src/test/java/ru/manrovich/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.manrovich.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
